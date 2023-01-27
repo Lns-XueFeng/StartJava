c61540ec6d1541f131763f46bd1cc5536d230dc7
Lns-XueFeng <1477792904@qq.com>
2023-01-27 18:52:30 +0800

    Initial commit

---
+++ src/Puppy.java
@@ -0,0 +1,23 @@
+public class Puppy {
+    int puppyAge;
+
+    public Puppy(String name){
+        System.out.println("The Puppy is " + name);
+    }
+
+    public void setPuppyAge(int age){
+        puppyAge = age;
+    }
+
+    public int getPuppyAge(){
+        System.out.println("Now PuppyAge is: " + puppyAge);
+        return puppyAge;
+    }
+
+    public static void main(String[] args) {
+        Puppy myPuppy = new Puppy("kaiQi");
+        myPuppy.setPuppyAge(5);
+        int nowAge = myPuppy.getPuppyAge();
+        System.out.println("nowAge: " + nowAge + "\nmyPuppy.puppyAge: " + myPuppy.puppyAge);
+    }
+}
